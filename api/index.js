48be0c9fa9230bb03d56691f5215632bb26357eb
Gigi <messigagilberto@gmail.com>
2026-06-17 16:06:56 +0000

    fix: convert api entry to plain JS to bypass Vercel tsc

--- api/index.js
+++ api/index.js
@@ -27,11 +27,7 @@ app.use(
   }),
 );
 
-app.use(cors({
-  credentials: true,
-  origin: true,
-}));
-
+app.use(cors({ credentials: true, origin: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
